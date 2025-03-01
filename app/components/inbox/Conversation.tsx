6a99246d9fd257c9866df0eff48fd56212b74297
Jenyfer Montiel <jenymontiel13@gmail.com>
2025-03-01 15:43:38 -0600

    09 inbox page(static)

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,11 @@
+const Conversation = () => {
+    return (
+        <div className="px-6 py-4 cursor pointer border border-pink-400 rounded-xl">
+            <p className="mb-6 text-xl">Billie Eilish</p>
+
+            <p className="text-airbnb-dark">Go to Conversation</p>
+        </div>
+    )
+}
+
+export default Conversation;

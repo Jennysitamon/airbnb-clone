cc611f21894776bf5e62a1123d4563fefa5f2a38
Jenyfer Montiel <jenymontiel13@gmail.com>
2025-03-19 13:12:22 -0600

    detail page for properties

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,35 +1,38 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
+import { useRouter } from "next/navigation";
 
 interface PropertyProps {
-    property: PropertyType
+    property: PropertyType,
 }
 
 const PropertyListItem: React.FC<PropertyProps> = ({
-    property
+    property,
 }) => {
-    return (
-        <div className="cursor-pointer">
+    const router = useRouter();
+    return(
+        <div 
+        onClick={() => router.push(`/properties/${property.id}`)}
+            className="cursor-pointer"
+        >
             <div className="relative overflow-hidden aspect-square rounded-xl">
-                <Image 
-                fill
-                src={property.image_url}
-                sizes="(max-width: 760px) 768px, (max-width: 1200px): 768px, 768px"
-                className="hover:scale-110 object-cover tansition h-full w-full"
-                alt="Casa de playa"
+            <Image
+                    fill
+                    src={property.image_url}
+                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+                    className="hover:scale-110 object-cover transition h-full w-full"
+                    alt="Beach house"
                 />
             </div>
-
             <div className="mt-2">
                 <p className="text-lg font-bold">{property.title}</p>
             </div>
-
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> Por noche</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
-
         </div>
     )
+    
 }
 
 export default PropertyListItem;

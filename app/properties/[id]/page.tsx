39da29e10cedc6e69782f3d016efb6af1521beb5
Jenyfer Montiel <jenymontiel13@gmail.com>
2025-03-20 10:15:34 -0600

    book property

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,29 +1,65 @@
 import Image from "next/image";
-import ReservationSidebar from "../../components/properties/ReservationSidebar";
+import Link from "next/link";
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 
-const PropertyDetailPage = () => {
+import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
+
+const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`);
+    const userId = await getUserId();
+
+    console.log('userId', userId);
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
-                <Image fill src="/playa_1.png" className="object-cover w-full h-full" alt="Beach house" />
+                <Image
+                    fill
+                    src={property.image_url}
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+                />
             </div>
-            <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">Property name</h1>
-                    <span className="mb-6 block text-lg text-gray-400"> 
-                        4 guests - 2 bedrooms - 1 bathroom
+                    <h1 className="mb-4 text-4xl">{property.title}</h1>
+
+                    <span className="mb-6 block text-lg text-pink-600">
+                        {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
                     </span>
+
                     <hr />
-                    <div className="py-6 flex items-center space-x-4">
-                        <Image src="/profile_pic_1.png" width={50} height={50} className="rounded-full" alt="The user name" />
-                        <p><strong>Billie Eilish</strong> is your host</p>
-                    </div>
+
+                    <Link 
+                        href={`/landlords/${property.landlord.id}`}
+                        className="py-6 flex items-center space-x-4"
+                    >
+                        {property.landlord.avatar_url && (
+                            <Image
+                                src={property.landlord.avatar_url}
+                                width={50}
+                                height={50}
+                                className="rounded-full"
+                                alt="The user name"
+                            />
+                        )}
+
+                        <p><strong>{property.landlord.name}</strong> is your host</p>
+                    </Link>
+
                     <hr />
+
                     <p className="mt-6 text-lg">
-                        El mejor de las csas de todos los tiempos la mejor de huamantla será!
+                        {property.description}
                     </p>
                 </div>
-                <ReservationSidebar />
+
+                <ReservationSidebar 
+                    property={property}
+                    userId={userId}
+                />
             </div>
         </main>
     )

36013543c4955e9368338cbaf2d4bb178c3e78fc
Jenyfer Montiel <jenymontiel13@gmail.com>
2025-03-24 23:41:02 -0600

    make it possible to connect

--- app/inbox/[id]/page.tsx
+++ app/inbox/[id]/page.tsx
@@ -1,9 +1,40 @@
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
+import { getUserId } from "../../lib/actions";
+import React, {useState, useEffect } from 'react';
+import apiService from "@/app/services/apiService";
+import { UserType } from "../page";
+import { getAccessToken } from "../../lib/actions";
 
-const ConversationPage = () => {
+export type MessageType = {
+    id: string;
+    name: string;
+    body: string;
+    conversationId: string;
+    sent_to: string;
+    created_by: UserType
+}
+
+const ConversationPage = async ({ params }: { params: {id: string }}) => {
+    const userId = await getUserId();
+    const token = await getAccessToken();
+    
+        if (!userId || !token) {
+            return (
+                <main className="max-w-[1500px] max-auto px-6 pb-12">
+                    <p>Tu necesitas autenticación...</p>
+                </main>
+            )
+        }
+
+    const conversation = await apiService.get(`/api/chat/${params.id}/`)
+
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6 mt-6">
-            <ConversationDetail />
+            <ConversationDetail 
+            token={token}
+            userId={userId}
+            conversation={conversation.conversation}
+            />
         </main>
     )
 }
